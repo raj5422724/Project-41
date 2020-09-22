c368131b632c99411e59b641f951aee095c7e84f
Satyam Raj <68588524+raj5422724@users.noreply.github.com>
2020-09-22 17:20:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var thunder, thunder1,thunder2;
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+var thunderCreatedFrame=0;
+
+function preload(){
+    thunder1 = loadImage("thunder1.png");
+    thunder2 = loadImage("thunder2.png");
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(400,700);
+    umbrella = new Umbrella(200,500);
+    umbrella.scale = 0.1;
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(0); 
+
+    //creating thunder
+    rand = Math.round(random(1,4));
+    if(frameCount%80===0){
+        thunderCreatedFrame=frameCount;
+        thunder = createSprite(random(10,370), random(10,30), 10, 10);
+        switch(rand){
+            case 1: thunder.addImage(thunder1);
+            break;
+            case 2: thunder.addImage(thunder2);
+            break; 
+            default: break;
+        }
+        thunder.scale = random(0.8,1)
+    }
+
+    if(thunderCreatedFrame + 10 ===frameCount && thunder){
+        thunder.destroy();
+    }
+
+    umbrella.display();
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY();
+    }
+
+    drawSprites();
+}   
